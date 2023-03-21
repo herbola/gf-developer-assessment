7cbdd36d156c94bbd926178349a3907a210307a5
Max Olofsson <max.olofsson@bonniernews.se>
2023-03-21 11:03:12 +0100

    Update app.js

--- worker/app.js
+++ worker/app.js
@@ -2,7 +2,7 @@ import { start, route } from "@bonniernews/b0rker";
 
 import validate from "./lib/lambdas/order/validate.js";
 import getOrCreateAccount from "./lib/lambdas/order/get-or-create-account.js";
-import createSubscription from "./lib/lambdas/order/get-or-create-subscription.js";
+import getOrCreateSubscription from "./lib/lambdas/order/get-or-create-subscription.js";
 
 const app = start({
   recipes: [
@@ -12,7 +12,7 @@ const app = start({
       sequence: [
         route(".validate.source", validate),
         route(".get-or-create.account", getOrCreateAccount),
-        route(".get-or-create.subscription", createSubscription),
+        route(".get-or-create.subscription", getOrCreateSubscription),
       ],
     },
   ],
